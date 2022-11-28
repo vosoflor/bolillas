bdae0b09e0f702735df51582a2911a176ad3faa9
Verónica Osorio Florián <vero_oso11@hotmail.com>
2022-11-28 12:55:37 -0500

    Primer intento clase crear rectangulo

--- main.py
+++ main.py
@@ -6,7 +6,6 @@ pg.init()
 #Creamos pantalla o surface para trabajar
 pantalla_principal = pg.display.set_mode( (800,600) ) #ventana y tamaño de ventana
 pg.display.set_caption("Bolillas juguetonas") #título de la ventana
-pantalla_principal.fill( (52,152,219) ) #Asignar color a la pantalla con formato de color RGB
 
 game_over = False #Variable para controlar bucle
 
@@ -16,6 +15,7 @@ while not game_over: #Bucle para ejecutar los fotogramas para el repintado de pa
         if eventos.type == pg.QUIT:
             game_over = True
         
+        pantalla_principal.fill( (52,152,219) ) #Asignar color a la pantalla con formato de color RGB
         pg.draw.rect(pantalla_principal,(192,57,43), (400-10,300-10,20,20)) #Se agrega rectángulo a la pantalla y los parámetros entregados son pantalla, color, ubicación y tamaño (ancho ubicación, alto uubicación, ancho tamaño, alto tamaño)
         pg.display.flip() #Método que añade a la pantalla los recursos que se van incluyendo
 
